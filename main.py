80e82b2111c3594d99acb2a83fa90847822786a4
blattmt <mblatt@gmail.com>
2025-05-06 02:33:03 -0400

    Final update allows for arguments, not just a hardcoded book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +7,11 @@ def get_book_text(filepath):
         return file_contents
     
 def main():
-    file = "./books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file = sys.argv[1]
 
     print("============ BOOKBOT ============")
     print(f'Analyzing book found at {file}')
